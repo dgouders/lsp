dgouders/lsp

commit 411a559c56fd5b3ce12407c19172019edaf16b7c

options: introduce toggle -V / --no-verify
Add a toggle option to completely turn off verification of
references.

This comes at some costs but I could imagine situations where users
just don't want to accept the cost of verification.

Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -2875,6 +2875,10 @@ static bool lsp_validate_ref_at_pos(regmatch_t pos)
 {
 	struct gref_t *gref = lsp_get_gref_at_pos(pos);
 
+	/* Return true if verification is turned off. */
+	if (!lsp_verify)
+		return true;
+
 	if (gref->valid == -1)
 		gref->valid = lsp_ref_is_valid(gref);
 
@@ -4477,6 +4481,15 @@ static void lsp_cmd_toggle_options()
 		else
 			lsp_maxx += 8;
 		break;
+	case 'V':
+		lsp_verify = !lsp_verify;
+
+		if (lsp_verify) {
+			lsp_prompt = "Verification of references turned ON.";
+		} else {
+			lsp_prompt = "Verification of references turned OFF.";
+		}
+		break;
 	} /* switch() */
 }
 
@@ -5356,6 +5369,8 @@ static void lsp_init()
 	lsp_verify_command = strdup("man -w \"%s\" > /dev/null 2>&1");
 
 	lsp_verify_with_apropos = false;
+
+	lsp_verify = true;
 
 	lsp_htable_entries = 100000;
 	lsp_grefs_count = 0;

doc/lsp-help.1
@@ -173,4 +173,6 @@ _
 _
 -n;toggle line numbering
 _
+-V;toggle verification of references
+_
 .TE

lsp.h
@@ -436,6 +436,7 @@ char	*lsp_reload_command;
 /* Command used to verify references. */
 char	*lsp_verify_command;
 bool	lsp_verify_with_apropos;
+bool	lsp_verify;
 
 /*
  * Further global variables.

commit 8bb309ea2fc73206c7d66c70795d652f51b59e30

options: Toggle -V / --no-verify also is a command line option
Commit 411a559c56fd (options: introduce toggle -V / --no-verify) was
incomplete in that the toggle should also be recognized as a command
line option.

Fixes: 411a559c56fd (options: introduce toggle -V / --no-verify)
Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -5204,6 +5204,7 @@ static void lsp_process_options(int argc, char *argv[])
 		{"search_string",	required_argument,	0, 's'},
 		{"version",		no_argument,		0, 'v'},
 		{"no-color",		no_argument,		0, '0'},
+		{"no-verify",		no_argument,		0, 'V'},
 		{"reload-command",	required_argument,	0, '1'},
 		{"verify-command",	required_argument,	0, '2'},
 		{"verify-with-apropos", no_argument,		0, '3'},
@@ -5211,7 +5212,7 @@ static void lsp_process_options(int argc, char *argv[])
 	};
 
 	while (1) {
-		opt = getopt_long(argc, argv, "achiIl:no:s:v",
+		opt = getopt_long(argc, argv, "achiIl:no:s:Vv",
 				  long_options, &long_index);
 
 		if (opt == -1)
@@ -5266,6 +5267,9 @@ static void lsp_process_options(int argc, char *argv[])
 		case 'v':
 			lsp_version();
 			exit(EXIT_SUCCESS);
+		case 'V':
+			lsp_verify = !lsp_verify;
+			break;
 		case 'h':
 		default:
 			lsp_usage(argv[0]);