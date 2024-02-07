dgouders/lsp

commit fa5240db7874b5816adc3ff4a9ae631c7ca949ff

open manpages: use MAN_PN to get the file names
Our prior way to find names of manual pages was to inspect the first
line of a manual page which -- in almost all cases -- should contain
the proper name twice.

But, there are cases where those names are of no use and cause real
harm to us -- docker is an example, using the name "docker(1)" for all
of its manual pages.

Luckily, man-db's man(1) provides MAN_PN containing the name of the
manual page.  But when we created childs executing man(1) we have no
access to their MAN_PN.

Try this hack to get the names of such manual pages:

* Create a script lsp_cat that outputs a heading line with the value
  of MAN_PN prior to the actual content of the manual page:

  <lsp-man-pn>lsp(1)</lsp-man-pn>

* Create child processes with PAGER=lsp_cat.

* Extract the name of the manual page from the first line of
  the incoming data.

For this, we built a more general read function lsp_file_do_read().
(Looks as if there was an error in lsp_file_read_block() anyway, by
adding the (possibly negative) result of read(2) to nread...)

Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -125,6 +125,11 @@ static void *lsp_realloc(void *ptr, size_t size)
  * Try to detect if file_t is a manual page.
  * Return a string xyz(n) if so, NULL otherwise.
  *
+ * We start by checking if we find an environment variable MAN_PN that gives us
+ * the name of the manual page.
+ *
+ * If that fails, we are falling back to our prior behavior:
+ *
  * We are testing the first line of the current file
  * for a pattern like:
  *
@@ -136,9 +141,18 @@ static char *lsp_detect_manpage()
 	char *regex_mid = " {2,}.+ {2,}";
 	char *ref_regex = lsp_search_ref_string;
 	char *regex_str;
+	char *name;
 	regex_t preg;
 	regmatch_t pmatch[1];
 	struct lsp_line_t *line = NULL;
+
+	/* Check for MAN_PN */
+	name = getenv("MAN_PN");
+
+	if (name != NULL) {
+		lsp_debug("%s: found MAN_PN=\"%s\"", __func__, name);
+		return strdup(name);
+	}
 
 	line = lsp_get_line_at_pos(0);
 	if (!line)
@@ -166,7 +180,7 @@ static char *lsp_detect_manpage()
 		return NULL;
 	}
 
-	char *name = strchr(line->normalized, ')');
+	name = strchr(line->normalized, ')');
 	size_t len = name + 1 - line->normalized;
 	line->normalized[len] = '\0';
 
@@ -1450,6 +1464,36 @@ static void lsp_file_data_ctor(size_t size_to_read)
 }
 
 /*
+ * Perform an actual read(2) with error handling.
+ */
+static ssize_t lsp_file_do_read(char *buffer_p, size_t size_to_read)
+{
+	ssize_t nread = read(cf->fd, buffer_p, size_to_read);
+
+	if (nread == -1) {
+		lsp_debug("%s: input file %s: %s",
+			  __func__, cf->name, strerror(errno));
+
+		/* When we read from ptmxfd given by forktty()
+		   we get EIO at the end of data.
+		   So, simulate normal EOF by setting nread = 0. */
+		if (errno == EIO)
+			nread = 0;
+		return nread;
+	}
+
+	/* Duplicate input to file given with -o */
+	if (lsp_ofile > 0)
+		write(lsp_ofile, buffer_p, nread);
+
+	if (nread < size_to_read)
+		lsp_debug("%s, pos %ld: read %ld bytes instead of %ld.",
+			  __func__, cf->seek, nread, size_to_read);
+
+	return nread;
+}
+
+/*
  * Add a block of data to the current file
  */
 static ssize_t lsp_file_read_block(size_t size_to_read)
@@ -1479,28 +1523,12 @@ static ssize_t lsp_file_read_block(size_t size_to_read)
 		nread = 1;
 	}
 
-	nread += read(cf->fd, buffer_p, size_to_read);
+	ssize_t ret = lsp_file_do_read(buffer_p, size_to_read);
 
-	if (nread == -1) {
-		lsp_debug("%s: input file %s: %s",
-			  __func__, cf->name, strerror(errno));
+	if (ret == -1)
+		return -1;
 
-		/* When we read from ptmxfd given by forktty()
-		   we get EIO at the end of data.
-		   So, simulate normal EOF by setting nread = 0. */
-		if (errno == EIO)
-			nread = 0;
-		else
-			return -1;
-	}
-
-	/* Duplicate input to file given with -o */
-	if (lsp_ofile > 0)
-		write(lsp_ofile, buffer_p, nread);
-
-	if (nread < size_to_read)
-		lsp_debug("%s, pos %ld: read %ld bytes instead of %ld.",
-			  __func__, cf->seek, nread, size_to_read);
+	nread += ret;
 
 	cf->seek += nread;
 
@@ -4188,6 +4216,81 @@ char** lsp_create_argv(char *format, char *str)
 	return argv;
 }
 
+/*
+ * At least man-db's man(1) sets MAN_PN to the name of the manual page and we
+ * could use it as our file name -- but this variable is set in the
+ * context of the child process and I don't know a way to get another processes
+ * environment variable.
+ *
+ * So, we use this hack to come to know the child's MAN_PN:
+ *
+ * - We execute the man(1) command with a script lsp_cat set as PAGER.
+ * - lsp_cat(1) outputs a line containing the value of the environment variable
+ *   MAN_PN, e.g.
+ *
+ *   <lsp-man_pn>fdopen(3)</lsp-man_pn>
+ *
+ *   heading the real content of the manual page.
+ *
+ * We read one line from the current fd to extract this embedded information
+ * from the real manual page.
+ *
+ * Return the found name or NULL on failure.
+ * The caller has to free() the name.
+ *
+ * fixme: currently, we don't return NULL or handle all possible errors.
+ *        We should detect if there is no such heading line.  We must then copy
+ *        the read data to cf->data, because it obviously was manpage content.
+ *        And, we should know how to proceed if our hack failed...
+ */
+static char *lsp_read_manpage_name()
+{
+	/* The heading line shouldn't exceed 256 byte -- famous last words. */
+	char name[256];
+	char *start;
+	char *end;
+	char *ret;
+
+	int i = 0;
+	ssize_t len;
+	char c = 0;
+
+	/* Read single bytes until we get a linefeed. */
+	while (c != '\n') {
+		if (i == 256)
+			lsp_error("%s: too long heading line...", __func__);
+
+		len = lsp_file_do_read(&c, 1);
+
+		if (len == 1) {
+			name[i++] = c;
+			continue;
+		}
+	}
+
+	name[i] = '\0';
+
+	/* Line is complete.  Extract the manpage name. */
+	start = strchr(name, '>');
+
+	if (start == NULL)
+		lsp_error("%s: didn't find end of starting <lsp_man_pn>", __func__);
+
+	start += 1;			/* Go next to <lsp-man-pn> */
+	end = strchr(start, '<');
+
+	if (end == NULL)
+		lsp_error("%s: didn't find start of final </lsp_man_pn>", __func__);
+
+	*end = '\0';			/* Cut off </lsp-man-pn> */
+
+	ret = strdup(start);
+
+	lsp_debug("%s: found MAN_PN = \"%s\"", __func__, ret);
+
+	return ret;
+}
+
 static void lsp_exec_man()
 {
 	/*
@@ -4207,7 +4310,7 @@ static void lsp_exec_man()
 		lsp_error("%s", strerror(errno));
 
 	if (pid == 0) {		/* child process */
-		putenv("PAGER=cat");
+		putenv("PAGER=lsp_cat");
 
 		char **e_argv = lsp_create_argv(lsp_reload_command, cf->name);
 
@@ -4227,6 +4330,9 @@ static void lsp_exec_man()
 	cf->blksize = statbuf.st_blksize;
 	// fixme: perhaps, use pagesize, here.
 	//cf->blksize = 1024;
+
+	/* Try to find a manpage name heading its content. */
+	char *name = lsp_read_manpage_name();
 
 	lsp_file_read_all();
 
@@ -4251,10 +4357,9 @@ static void lsp_exec_man()
 			  __func__, (intmax_t)ret_pid, (intmax_t)pid);
 	}
 
-	/*
-	 * Set file name to correct name of manual page.
-	 */
-	char *name = lsp_detect_manpage();
+	if (name == NULL)
+		name = lsp_detect_manpage();
+
 	if (name == NULL || LSP_STR_EQ(cf->name, name)) {
 		free(name);
 		return;

lsp.h
@@ -175,6 +175,7 @@ static void			lsp_file_create_toc(void);
 static struct file_t *		lsp_file_ctor(void);
 static void			lsp_file_data_ctor(size_t);
 static void			lsp_file_data_dtor(struct data_t *);
+static ssize_t			lsp_file_do_read(char *, size_t);
 static void			lsp_file_do_reload(void);
 static void			lsp_file_dtor(struct file_t *);
 static struct file_t *		lsp_file_find(char *);
@@ -269,6 +270,7 @@ static bool			lsp_pos_is_at_bol(off_t);
 static bool			lsp_pos_is_current_page(off_t);
 static bool			lsp_pos_is_toc(off_t);
 static void			lsp_process_env_open(void);
+static char *			lsp_read_manpage_name(void);
 static void *			lsp_realloc(void *, size_t);
 static bool			lsp_ref_is_valid(struct gref_t *);
 static void			lsp_screen_line_bw(int);

lsp_cat
@@ -1,0 +1,27 @@
+#!/bin/bash
+#
+# lsp_cat - output a heading line containing $MAN_PN prior to the
+#           actual content of a manual page.
+#
+# Copyright (C) 2024, Dirk Gouders
+#
+# This file is part of lsp.
+#
+# lsp is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 2 of the License, or (at your option) any later
+# version.
+#
+# lsp is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# lsp. If not, see <https://www.gnu.org/licenses/>.
+#
+
+if [ "$MAN_PN" != "" ] ; then
+    echo "<lsp-man-pn>$MAN_PN</lsp-man-pn>"
+fi
+
+exec cat

meson.build
@@ -18,4 +18,10 @@ executable(
         install : true
 )
 
+install_data(
+        'lsp_cat',
+        install_dir: 'bin',
+        install_mode: 'rwxr-xr-x'
+)
+
 subdir('doc')

commit 4c2773a7511a2ab91276db76482d6ccfdcec4c07

build: fix warnings
"Ohne Worte" was a frequent subscription of cartoons when I was
young...

- lsp_file_set_blksize() doesn't return anything.

- lsp_file_do_read() should be handed over an unsigned char*.

Fixes: fa5240db7874 (open manpages: use MAN_PN to get the file names)
Fixes: c1d71376acb7 (lsp: handle files with inaccurate sizes)
Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -1435,7 +1435,7 @@ static void lsp_file_close()
  * fixme: Perhaps, we later need to tweak this and use pagesize or something
  *        else.
  */
-static blksize_t lsp_file_set_blksize()
+static void lsp_file_set_blksize()
 {
 	struct stat statbuf;
 
@@ -1543,7 +1543,7 @@ static void lsp_file_data_ctor(size_t size_to_read)
 /*
  * Perform an actual read(2) with error handling.
  */
-static ssize_t lsp_file_do_read(char *buffer_p, size_t size_to_read)
+static ssize_t lsp_file_do_read(unsigned char *buffer_p, size_t size_to_read)
 {
 	ssize_t nread = read(cf->fd, buffer_p, size_to_read);
 
@@ -4400,7 +4400,7 @@ static char *lsp_read_manpage_name()
 
 	int i = 0;
 	ssize_t len;
-	char c = 0;
+	unsigned char c = 0;
 
 	/* Read single bytes until we get a linefeed. */
 	while (c != '\n') {

lsp.h
@@ -183,7 +183,7 @@ static void			lsp_file_create_toc(void);
 static struct file_t *		lsp_file_ctor(void);
 static void			lsp_file_data_ctor(size_t);
 static void			lsp_file_data_dtor(struct data_t *);
-static ssize_t			lsp_file_do_read(char *, size_t);
+static ssize_t			lsp_file_do_read(unsigned char *, size_t);
 static void			lsp_file_do_reload(void);
 static void			lsp_file_dtor(struct file_t *);
 static struct file_t *		lsp_file_find(char *);
@@ -205,7 +205,7 @@ static void			lsp_file_reset(void);
 static void			lsp_file_ring_dtor(void);
 static void			lsp_file_search_next(lsp_mode_t);
 static void			lsp_file_search_prev(lsp_mode_t);
-static blksize_t		lsp_file_set_blksize(void);
+static void			lsp_file_set_blksize(void);
 static void			lsp_file_set_current_match(regmatch_t);
 static void			lsp_file_set_pos(off_t);
 static void			lsp_file_set_prev_line(void);