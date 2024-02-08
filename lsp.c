dgouders/lsp

commit c6ce5d470481d965115f634b60d1d3268e47e5c1

strings: finer distinguish from pure data
This patch is the next step in distinguishing between strings and
input data where null-characters could be part of the payload.

For this, two new functions lsp_mdup() and lsp_mdup2str() were
introduced.  The former duplicates plain data, the latter does the
same but finally builds a string appending a null-character.

One might ask: "Why not use strndup()?" and the answer is that
strndup() copies *at most* n bytes but we need copies of *exactly*
n bytes.

Further, lsp_to_lower() was modified to modify the given string
in-place and no longer return a duplicate.  This was done, because it
was more convenient to prepare a string before calling lsp_to_lower().

With all that done, we ensure that names passed to *gref*-functions
are guaranteed to be strings, gaining an uncomplicated
handling.

At other places: use the new lsp_mdup*-functions where applicable.

Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -86,6 +86,29 @@
 #include "lsp.h"
 
 /*
+ * Return a duplicate of data at *src of length len.
+ */
+static char *lsp_mdup(const char *src, size_t len)
+{
+	char *mem = lsp_malloc(len);
+
+	memcpy(mem, src, len);
+
+	return mem;
+}
+
+/*
+ * Duplicate data at *src of the given length len to a string.
+ */
+static char *lsp_mdup2str(const char *src, size_t len)
+{
+	char *str = lsp_malloc(len + 1);
+	memcpy(str, src, len);
+	str[len] = '\0';
+	return str;
+}
+
+/*
  * Our memory allocators with error handling.
  */
 static void *lsp_malloc(size_t size)
@@ -185,14 +208,13 @@ static char *lsp_detect_manpage(bool use_env)
 		return NULL;
 	}
 
-	name = strchr(line->normalized, ')');
+	name = memchr(line->normalized, ')', line->nlen);
 	size_t len = name + 1 - line->normalized;
-	line->normalized[len] = '\0';
 
-	if (lsp_man_case_sensitivity)
-		name = strdup(line->normalized);
-	else
-		name = lsp_to_lower(line->normalized);
+	name = lsp_mdup2str(line->normalized, len);
+
+	if (!lsp_man_case_sensitivity)
+		lsp_to_lower(name);
 
 	lsp_debug("%s: manual page detected \"%s\"", __func__, name);
 
@@ -2000,7 +2022,7 @@ static int lsp_open_file(const char *name)
 			cf->fd = open(cf->name, 0, "r");
 		} else {
 			lsp_debug("%s: opening replacement file \"%s\"", __func__, buffer);
-			cf->rep_name = strdup(buffer);
+			cf->rep_name = lsp_mdup(buffer, nread);
 			cf->fd = open(cf->rep_name, 0, "r");
 		}
 	}
@@ -2433,35 +2455,32 @@ static int lsp_gref_henter(struct gref_t *gref_p)
 }
 
 /*
- * Duplicate the given string to one with all lowercase chars.
+ * Translate the given string to one with all lowercase chars.
  */
-static char *lsp_to_lower(char *str)
+static void lsp_to_lower(char *str)
 {
 	int i;
-	char *new_str = lsp_malloc(strlen(str) + 1);
+	size_t len = strlen(str);
 
-	for (i = 0; str[i]; i++)
-		new_str[i] = tolower(str[i]);
+	for (i = 0; i < len; i++)
+		str[i] = tolower(str[i]);
 
-	new_str[i] = '\0';
-
-	return new_str;
+	return;
 }
 
 /*
  * Search for a global reference.
  * Add it if it does not exist.
  */
-static struct gref_t *lsp_gref_search(char *name)
+static struct gref_t *lsp_gref_search(const char *name)
 {
 	struct gref_t *ptr;
 	char *tmp_name;
 
-	if (!lsp_man_case_sensitivity) {
-		tmp_name = lsp_to_lower(name);
-	} else {
-		tmp_name = strdup(name);
-	}
+	tmp_name = strdup(name);
+
+	if (!lsp_man_case_sensitivity)
+		lsp_to_lower(tmp_name);
 
 	if (lsp_grefs) {
 		ptr = lsp_gref_find(tmp_name);
@@ -2482,7 +2501,7 @@ static struct gref_t *lsp_gref_search(char *name)
 
 	/* gref not found: add it. */
 	ptr = lsp_malloc(sizeof(struct gref_t));
-	ptr->name = tmp_name;   /* strdup() happend in lsp_to_lower()! */
+	ptr->name = tmp_name;   /* duplication already done. */
 	ptr->valid = -1;	/* not yet validated */
 
 	if (lsp_grefs)
@@ -3342,10 +3361,8 @@ static size_t lsp_line_get_matches(const struct lsp_line_t *line, regmatch_t **p
 	 *
 	 * Duplicate the normalized line and remove the final \n.
 	 */
-	char *sstring = strdup(line->normalized);
+	char *sstring = lsp_mdup(line->normalized, line->nlen - 1);
 	size_t slen = line->nlen - 1;
-
-	sstring[slen] = '\0';
 
 	/* Allocate memory for max possible number of matches and that
 	   should be the number of bytes in the line.
@@ -4641,7 +4658,7 @@ static char *lsp_man_get_section(off_t pos)
 	if (line->pos == 0)
 		section_name = strdup("_start_of_manual_page_");
 	else
-		section_name = strdup(line->normalized);
+		section_name = lsp_mdup2str(line->normalized, line->nlen);
 
 	lsp_line_dtor(line);
 
@@ -4944,7 +4961,7 @@ static char *lsp_cmd_select_file()
 			if (LSP_STR_EQ(line->raw, "*stdin*")) {
 				file_name = strdup("");
 			} else
-				file_name = strdup(line->raw);
+				file_name = lsp_mdup2str(line->raw, line->len);
 
 			lsp_line_dtor(line);
 			return file_name;
@@ -5087,10 +5104,11 @@ static void lsp_apropos_create_grefs()
 
 		assert(end_ref != (void *)1);
 
-		/* Terminate the line string after the reference. */
-		*end_ref = '\0';
+		char *ref_name = lsp_mdup2str(line->normalized, end_ref - line->normalized);
 
-		gref = lsp_gref_search(line->normalized);
+		gref = lsp_gref_search(ref_name);
+
+		free(ref_name);
 
 		assert(gref != NULL);
 

lsp.h
@@ -220,7 +220,7 @@ static struct gref_t *		lsp_gref_find(char *);
 static int			lsp_gref_henter(struct gref_t *);
 static void			lsp_goto_bol(void);
 static void			lsp_grefs_dtor(void);
-static struct gref_t *		lsp_gref_search(char *);
+static struct gref_t *		lsp_gref_search(const char *);
 static bool			lsp_has_man_placeholders(const char *);
 static void			lsp_init(void);
 static void			lsp_init_cmd_input(void);
@@ -249,6 +249,8 @@ static int			lsp_man_goto_section(char *);
 static void			lsp_man_reposition(char *);
 static uint			lsp_mblen(const char *, size_t);
 static size_t			lsp_mbtowc(wchar_t *, const char *, size_t);
+static char *			lsp_mdup2str(const char *, size_t);
+static char *			lsp_mdup(const char *, size_t);
 static bool			lsp_mode_is_highlight(void);
 static bool			lsp_mode_is_refs(void);
 static bool			lsp_mode_is_search(void);
@@ -293,7 +295,7 @@ static void			lsp_set_no_current_match(void);
 static size_t			lsp_skip_bsp(const char *);
 static size_t			lsp_skip_sgr(const char *);
 static size_t			lsp_skip_to_payload(const char *);
-static char *			lsp_to_lower(char *);
+static void			lsp_to_lower(char *);
 static struct toc_node_t *	lsp_pos_to_toc(off_t);
 static void			lsp_toc_bw(size_t);
 static void			lsp_toc_dtor(struct file_t *);

commit 4212ab47ea065d6224c3d2158c7b53d36468bd77

preprocessor: fix opening replacement file
When opening a replacement file, commit
c6ce5d470481 (strings: finer distinguish from pure data)
used lsp_mdup() where lsp_mdup2str() would have been correct.

Our pre-push testing gets better but was improved too late to help
with this one.

Fixes: c6ce5d470481 (strings: finer distinguish from pure data)
Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -2035,8 +2035,8 @@ static int lsp_open_file(const char *name)
 			lsp_debug("%s: no replacement file for \"%s\"", __func__, cf->name);
 			cf->fd = open(cf->name, 0, "r");
 		} else {
-			lsp_debug("%s: opening replacement file \"%s\"", __func__, buffer);
-			cf->rep_name = lsp_mdup(buffer, nread);
+			cf->rep_name = lsp_mdup2str(buffer, nread);
+			lsp_debug("%s: opening replacement file \"%s\"", __func__, cf->rep_name);
 			cf->fd = open(cf->rep_name, 0, "r");
 		}
 	}