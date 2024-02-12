dgouders/lsp

commit c1d71376acb72431bee90bb1e1917e836c55cf3e

lsp: handle files with inaccurate sizes
Reported file sizes are inaccurate for kernel generated pseudofiles
(see inode(7):NOTES) and we don't handle them correctly.

Fix this by _not_ using reported sizes for files in /proc and /sys.
Handle those files like input from stdin and set the size after we
reached EOF.

For this, a new functions lsp_file_set_size() has been created.

While we were at it, lsp_set_fd_blksize() was renamed accordingly and
it is now called from _all_ relevant locations.

Also, we needed to use strncmp(3) and two macros similar to those for
strcmp(3) were added.

Fixes issue #18.

Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -1384,15 +1384,19 @@ static void lsp_file_close()
 	}
 }
 
-static blksize_t lsp_set_fd_blksize()
+/*
+ * Set buffer size for current file for read operations.
+ *
+ * fixme: Perhaps, we later need to tweak this and use pagesize or something
+ *        else.
+ */
+static blksize_t lsp_file_set_blksize()
 {
 	struct stat statbuf;
 
 	fstat(cf->fd, &statbuf);
 
 	cf->blksize = statbuf.st_blksize;
-	// fixme: perhaps, use pagesize, here.
-	//cf->blksize = 1024;
 }
 
 /*
@@ -1406,7 +1410,7 @@ static void lsp_file_inject_line(const char *line)
 	lsp_file_add_line(line);
 
 	cf->size = LSP_FSIZE_UNKNOWN;
-	lsp_set_fd_blksize();
+	lsp_file_set_blksize();
 	cf->data->buffer = lsp_realloc(cf->data->buffer, cf->blksize);
 }
 
@@ -1821,8 +1825,6 @@ static void lsp_open_cterm()
  */
 static void lsp_file_init_stdin()
 {
-	struct stat statbuf;
-
 	lsp_debug("No input files given -- checking stdin...");
 
 	if (isatty(STDIN_FILENO))
@@ -1832,10 +1834,8 @@ static void lsp_file_init_stdin()
 	   Let's use the the empty string. */
 	lsp_file_add("", 0);
 
-	fstat(STDIN_FILENO, &statbuf);
-
 	cf->size = LSP_FSIZE_UNKNOWN;
-	cf->blksize = statbuf.st_blksize;
+	lsp_file_set_blksize();
 
 	/* Move stdin to something > 2 and and then use the controlling terminal
 	 * as the one we use for the user to communicate with us. */
@@ -2025,12 +2025,55 @@ static void lsp_file_move_here(struct file_t *file_p)
 }
 
 /*
+ * Set size of current file.
+ */
+static void lsp_file_set_size()
+{
+	struct stat statbuf;
+	char *path;
+
+	if (fstat(cf->fd, &statbuf) == -1)
+		lsp_error("%s: %s", cf->name, strerror(errno));
+
+	if (!(S_ISREG(statbuf.st_mode) || S_ISFIFO(statbuf.st_mode)))
+		lsp_error("%s: %s: unsupported file type.",
+			  __func__, cf->name);
+
+	path = realpath(cf->name, NULL);
+	if (path == NULL ) {
+		lsp_debug("%s: couldn't get realpath(3) for %s",
+			  __func__, cf->name);
+		cf->size = LSP_FSIZE_UNKNOWN;
+		return;
+	}
+
+	/*
+	 * The size reported for some files (e.g. kernel generated pseudofiles)
+	 * is not accurate.  We handle them like stdin: unknown size until
+	 * read(2) hits EOF.
+	 */
+	if (LSP_STRN_EQ(path, "/proc/", 6) ||
+	    LSP_STRN_EQ(path, "/sys/", 5)) {
+		cf->size = LSP_FSIZE_UNKNOWN;
+		goto out;
+	}
+
+	/* We know sizes only of regular files. */
+	if (S_ISREG(statbuf.st_mode))
+		cf->size = statbuf.st_size;
+	else
+		cf->size = LSP_FSIZE_UNKNOWN;
+
+out:
+	free(path);
+}
+
+/*
  * Initialize ring of input files
  * Open the first given file (or stdin) for reading.
  */
 static void lsp_file_init_ring()
 {
-	struct stat statbuf;
 	struct file_t *ring_start = cf;
 
 	if (cf == NULL) {
@@ -2048,20 +2091,9 @@ static void lsp_file_init_ring()
 		if (cf->fd == -1)
 			lsp_error("%s: %s", cf->name, strerror(errno));
 
-		if (fstat(cf->fd, &statbuf) == -1)
-			lsp_error("%s: %s", cf->name, strerror(errno));
+		lsp_file_set_size();
 
-		if (!(S_ISREG(statbuf.st_mode) || S_ISFIFO(statbuf.st_mode)))
-			lsp_error("%s: %s: unsupported file type.",
-				  __func__, cf->name);
-
-		/* We know sizes only of regular files. */
-		if (S_ISREG(statbuf.st_mode))
-			cf->size = statbuf.st_size;
-		else
-			cf->size = LSP_FSIZE_UNKNOWN;
-
-		cf->blksize = statbuf.st_blksize;
+		lsp_file_set_blksize();
 
 		lsp_file_add_block();
 
@@ -4358,7 +4390,7 @@ static void lsp_exec_man()
 	/* parent process */
 	cf->size = LSP_FSIZE_UNKNOWN;
 
-	lsp_set_fd_blksize();
+	lsp_file_set_blksize();
 
 	/* Try to find a manpage name heading its content. */
 	char *name = lsp_read_manpage_name();
@@ -4976,11 +5008,7 @@ static void lsp_cmd_create_apropos()
 	cf->fp = fp;
 	cf->fd = fileno(fp);
 
-	struct stat statbuf;
-	fstat(STDIN_FILENO, &statbuf);
-
-	cf->blksize = statbuf.st_blksize;
-
+	lsp_file_set_blksize();
 	lsp_file_add_block();
 
 	if (lsp_verify_with_apropos)

lsp.h
@@ -30,6 +30,9 @@
 #define LSP_STR_EQ(a, b) (strcmp(a, b) == 0)
 #define LSP_STR_NEQ(a, b) (strcmp(a, b) != 0)
 
+#define LSP_STRN_EQ(a, b, l) (strncmp(a, b, l) == 0)
+#define LSP_STRN_NEQ(a, b, l) (strncmp(a, b, l) != 0)
+
 /*
  * We store each file's content in a ring of buffers of size blksize.
  */
@@ -197,9 +200,11 @@ static void			lsp_file_reset(void);
 static void			lsp_file_ring_dtor(void);
 static void			lsp_file_search_next(lsp_mode_t);
 static void			lsp_file_search_prev(lsp_mode_t);
+static blksize_t		lsp_file_set_blksize(void);
 static void			lsp_file_set_current_match(regmatch_t);
 static void			lsp_file_set_pos(off_t);
 static void			lsp_file_set_prev_line(void);
+static void			lsp_file_set_size(void);
 static void			lsp_file_toc_add(const struct lsp_line_t *, int);
 static void			lsp_file_ungetch(void);
 static void			lsp_files_list(void);
@@ -284,7 +289,6 @@ static regmatch_t		lsp_search_file_next(void);
 static regmatch_t		lsp_search_find_prev_match(struct lsp_line_t **);
 static regmatch_t		lsp_search_next(void);
 static regmatch_t		lsp_search_toc_next(void);
-static blksize_t		lsp_set_fd_blksize(void);
 static void			lsp_set_no_current_match(void);
 static size_t			lsp_skip_bsp(const char *);
 static size_t			lsp_skip_sgr(const char *);

commit 22222326966987c238f893d447268842f8ca9d8f

lsp_file_init_stdin: set cf->fd before setting blksize
This error popped up only with optimization: when input comes from
stdin we must call lsp_file_set_blksize() _after_ cf has a valid fd.

Fis this by moving the call after STDIN_FILENO has been duplicated.

Also, add error handling to lsp_file_set_blksize().

Fixes: c1d71376acb7 (lsp: handle files with inaccurate sizes)
Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -1487,9 +1487,13 @@ static void lsp_file_close()
  */
 static void lsp_file_set_blksize()
 {
+	int ret;
 	struct stat statbuf;
 
-	fstat(cf->fd, &statbuf);
+	ret = fstat(cf->fd, &statbuf);
+
+	if (ret == -1)
+		lsp_error("%s: fstat(2): %s", __func__, strerror(errno));
 
 	cf->blksize = statbuf.st_blksize;
 }
@@ -1945,12 +1949,13 @@ static void lsp_file_init_stdin()
 	lsp_file_add("", 0);
 
 	cf->size = LSP_FSIZE_UNKNOWN;
-	lsp_file_set_blksize();
 
 	/* Move stdin to something > 2 and and then use the controlling terminal
 	 * as the one we use for the user to communicate with us. */
 	cf->fd = dup(STDIN_FILENO);
 	close(STDIN_FILENO);
+
+	lsp_file_set_blksize();
 
 	if (cf->fd <= 2)
 		lsp_debug("%s: file descriptor did not become > 2.", __func__);