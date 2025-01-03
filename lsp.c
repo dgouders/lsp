dgouders/lsp

commit b3e70f042af33173ee363c83b9dc3c05ff9b501b

lsp_file_reload: remove superfluous if-statement
The if-statement isn't needed, because the function is only called for
manual pages.

Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -5094,38 +5094,38 @@ static void lsp_man_reposition(char *section)
 
 static void lsp_file_reload()
 {
-	/* We reload manual pages only. */
-	if (cf->ftype & LSP_FTYPE_MANPAGE) {
-		char *saved_man_section = lsp_man_get_section(cf->page_first);
+	char *saved_man_section;
 
-		lsp_file_reset();
+	if (lsp_is_manpage())
+		saved_man_section = lsp_man_get_section(cf->page_first);
 
-		lsp_exec_man();
+	lsp_file_reset();
 
-		/* If there was a TOC all its entries now have invalid
-		   pointers (at a high possibility).  Rebuild it. */
-		if (cf->toc) {
-			lsp_mode_t old_mode;
+	lsp_exec_man();
 
-			lsp_toc_dtor(cf);
-			/* A TOC must be created in neutral mode (!toc).
-			   But we also want to stay in TOC mode if this is where
-			   the resize happened. */
-			old_mode = cf->mode;
-			lsp_mode_unset_toc();
-			lsp_toc_ctor();
-			cf->mode = old_mode;
-		}
+	/* If there was a TOC all its entries now have invalid
+	   pointers (at a high possibility).  Rebuild it. */
+	if (cf->toc) {
+		lsp_mode_t old_mode;
 
-		cf->do_reload = false;
-
-		lsp_man_reposition(saved_man_section);
-
-		free(saved_man_section);
-
-		cf->page_first = lsp_pos;
-		lsp_set_no_current_match();
+		lsp_toc_dtor(cf);
+		/* A TOC must be created in neutral mode (!toc).
+		   But we also want to stay in TOC mode if this is where
+		   the resize happened. */
+		old_mode = cf->mode;
+		lsp_mode_unset_toc();
+		lsp_toc_ctor();
+		cf->mode = old_mode;
 	}
+
+	cf->do_reload = false;
+
+	lsp_man_reposition(saved_man_section);
+
+	free(saved_man_section);
+
+	cf->page_first = lsp_pos;
+	lsp_set_no_current_match();
 }
 
 /*

commit 14eb42d657a38b1221c9633668a6cbeb6112a028

saved_man_section: fix warning maybe uninitialized
Currently, the function lsp_file_reload() is called only for manual
pages; there is no need to check for manual pages and this check
produces a warning.

Fixes: b3e70f042af3 (lsp_file_reload: remove superfluous if-statement)
Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -5171,12 +5171,15 @@ static void lsp_man_reposition(char *section)
 	}
 }
 
+/*
+ * Currently, only manual pages get reloaded -- no need to check if the current
+ * file (cf) is a manual page.
+ */
 static void lsp_file_reload()
 {
 	char *saved_man_section;
 
-	if (lsp_is_manpage())
-		saved_man_section = lsp_man_get_section(cf->page_first);
+	saved_man_section = lsp_man_get_section(cf->page_first);
 
 	lsp_file_reset();
 