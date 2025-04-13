dgouders/lsp

commit 5f7d2f95d245d32f547df8d1fc83acec4fc5fdbf

lsp_cmd_reload: introduce new command 'r'eload
This command currently supports only regular files and
rereads them.

The bigger idea was to support restarting parent processes, e.g.
`git diff' which could be useful while polishing commits.

But, this needs much more time and thoughts and meanwhile, other
things like fixing real bugs became more important.

Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -2046,6 +2046,7 @@ static void lsp_file_init_stdin()
 	lsp_file_add("", 0);
 
 	cf->size = LSP_FSIZE_UNKNOWN;
+	cf->ftype |= LSP_FTYPE_STDIN;
 
 	/* Move stdin to something > 2 and and then use the controlling terminal
 	 * as the one we use for the user to communicate with us. */
@@ -3129,6 +3130,84 @@ static char *lsp_search_compile_regex(lsp_mode_t search_mode)
 static void lsp_set_no_current_match()
 {
 	cf->current_match = lsp_no_match;
+}
+
+static bool lsp_file_is_regular()
+{
+	return cf->ftype & LSP_FTYPE_REGULAR;
+}
+
+static bool lsp_file_is_stdin()
+{
+	return cf->ftype & LSP_FTYPE_STDIN;
+}
+
+/*
+ * Read content of file up to given pos or EOF, if the file is smaller.
+ */
+static void lsp_file_read_to_pos(off_t pos)
+{
+	while (!LSP_EOF && cf->seek < pos)
+		lsp_file_add_block();
+}
+
+/*
+ * Check if the given file is (still) readable.
+ */
+static bool lsp_is_readable(char *path)
+{
+	return access(path, R_OK) == 0;
+}
+
+/*
+ * Reread current regular file and try to position to the page last shown.
+ */
+static void lsp_file_reread()
+{
+	/* Save position of last page shown. */
+	off_t old_page_first = cf->page_first;
+
+	if (!lsp_is_readable(cf->name)) {
+		lsp_prompt = "File is no longer readable.";
+		lsp_file_set_pos(cf->page_first);
+		return;
+	}
+
+	lsp_file_reset();
+	lsp_file_init();
+
+	/* Try to reread to the position we displayed last. */
+	lsp_file_read_to_pos(old_page_first);
+
+	lsp_debug("%s: reread file %s to pos %ld", __func__, cf->name, cf->size);
+
+	/* If the file is now smaller, position to its last page. */
+	if (cf->seek <= old_page_first) {
+		lsp_cmd_goto_end();
+		cf->page_first = lsp_pos;
+	} else
+		cf->page_first = old_page_first;
+
+	lsp_file_set_pos(cf->page_first);
+	return;
+}
+
+/*
+ * Reload content of current file.
+ *
+ * Currently, explicit reloading is supported only for regular files.
+ *
+ */
+static void lsp_cmd_reload()
+{
+	if (lsp_file_is_regular()) {
+		lsp_file_reread();
+		return;
+	}
+
+	lsp_file_set_pos(cf->page_first);
+	lsp_prompt = lsp_reload_not_supported;
+	return;
 }
 
 /*
@@ -6067,6 +6146,10 @@ static void lsp_workhorse()
 					return;
 				}
 			}
+			lsp_display_page();
+			break;
+		case 'r':
+			lsp_cmd_reload();
 			lsp_display_page();
 			break;
 		case ERR:

doc/lsp-help.1
@@ -45,6 +45,8 @@ T}
 _
 h;Show this online help.
 _
+r;Reload regular file.
+_
 .T&
 cfI s.
 Handling manual pages

doc/lsp.1
@@ -586,6 +586,14 @@ close help file.
 .IP "\[bu] In file selection:"
 exit selection without selecting a file; stay at the former one.
 .RE
+.
+.TP
+.
+.B r
+.br
+Reload current file.
+.IP
+(Currently only for regular files.)
 .\"--------------------------------------------------------------------
 .SH Environment
 .\"--------------------------------------------------------------------

lsp.h
@@ -166,6 +166,7 @@ static void			lsp_cmd_goto_start(void);
 static void			lsp_cmd_kill_file(void);
 static void			lsp_cmd_mouse(void);
 static void			lsp_cmd_open_manpage(void);
+static void			lsp_cmd_reload(void);
 static void			lsp_cmd_resize(void);
 static void			lsp_cmd_search(bool);
 static void			lsp_cmd_search_bw(lsp_mode_t);
@@ -206,6 +207,8 @@ static void			lsp_file_init(void);
 static void			lsp_file_init_ring(void);
 static void			lsp_file_init_stdin(void);
 static void			lsp_file_inject_line(const char *);
+static bool			lsp_file_is_regular(void);
+static bool			lsp_file_is_stdin(void);
 static void			lsp_file_kill(void);
 static void			lsp_file_move_here(struct file_t *);
 static int			lsp_file_peek_bw(void);
@@ -213,7 +216,9 @@ static int			lsp_file_peek_fw(void);
 static size_t			lsp_file_pos2line(off_t);
 static void			lsp_file_read_all(void);
 static ssize_t			lsp_file_read_block(size_t);
+static void			lsp_file_read_to_pos(off_t);
 static void			lsp_file_reload(void);
+static void			lsp_file_reread(void);
 static void			lsp_file_reset(void);
 static void			lsp_file_ring_dtor(void);
 static regmatch_t		lsp_file_search_next(void);
@@ -255,6 +260,7 @@ static bool			lsp_is_a_match(regmatch_t);
 static bool			lsp_is_at_bol(void);
 static bool			lsp_is_manpage(void);
 static bool			lsp_is_no_match(regmatch_t);
+static bool			lsp_is_readable(char *);
 static bool			lsp_is_sgr_sequence(const char *);
 static void			lsp_line_add_wlines(struct lsp_line_t *);
 static size_t			lsp_line_count_words(struct lsp_line_t *);
@@ -431,8 +437,9 @@ enum lsp_flag {
 typedef enum lsp_flag lsp_flag_t;
 
 enum lsp_ftype {
-	LSP_FTYPE_OTHER = 0,
+	LSP_FTYPE_OTHER   = 0,
 	LSP_FTYPE_MANPAGE = 1,
+	LSP_FTYPE_STDIN   = 2,	/* We were started with data coming from stdin. */
 	LSP_FTYPE_REGULAR = 4
 };
 
@@ -550,6 +557,7 @@ unsigned char lsp_shift;
 
 /* xxx put into lsp_init() or so */
 char	*lsp_not_found = "Pattern not found";
+char    *lsp_reload_not_supported = "Reload not supported.";
 
 /* String from LSP_OPEN or LESSOPEN environment variable. */
 char *lsp_env_open;

commit fae8a558b333b164d30afb784bb6b01bf8c743b7

lsp_file_reread: use lsp_file_set_pos_bol() instead of lsp_file_set_pos()
Re-reading regular files is buggy when the content changes
in a way that the old cf->page_first position doesn't stay at the
beginning of a line.

Fix that by using lsp_file_set_pos_bol() to ensure repositioning lands
at the beginning of a line.

Further, there is no need for the variable old_page_first, because
cf->page_first won't be modified by any function that we use.

Fixes: 5f7d2f9 (lsp_cmd_reload: introduce new command 'r'eload, 2025-01-03)
Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -3330,9 +3330,6 @@ static bool lsp_is_readable(char *path)
  */
 static void lsp_file_reread()
 {
-	/* Save position of last page shown. */
-	off_t old_page_first = cf->page_first;
-
 	if (!lsp_is_readable(cf->name)) {
 		lsp_prompt = "File is no longer readable.";
 		lsp_file_set_pos(cf->page_first);
@@ -3343,18 +3340,17 @@ static void lsp_file_reread()
 	lsp_file_init();
 
 	/* Try to reread to the position we displayed last. */
-	lsp_file_read_to_pos(old_page_first);
+	lsp_file_read_to_pos(cf->page_first);
 
 	lsp_debug("%s: reread file %s to pos %ld", __func__, cf->name, cf->size);
 
 	/* If the file is now smaller, position to its last page. */
-	if (cf->seek <= old_page_first) {
+	if (cf->seek <= cf->page_first) {
 		lsp_cmd_goto_last_page();
 		cf->page_first = lsp_pos;
-	} else
-		cf->page_first = old_page_first;
+	}
 
-	lsp_file_set_pos(cf->page_first);
+	lsp_file_set_pos_bol(cf->page_first);
 	return;
 }
 