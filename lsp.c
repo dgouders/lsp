dgouders/lsp

commit fcf742f6cdaee24513b6b37e2203873720bbfe07

lsp/display_page: always write complete page
We are close to restarting parent processes and it is time to take
care for cases when content we reload, changed.

One such case would be when the reloaded content of a file shrank --
to zero in the extreme.  In such cases, there might be content on the
screen that disappeared and we have to show that.

Fix this in lsp_display_page(): don't check if we reached EOF to save
some cycles but always fill the whole screen.

Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -4164,14 +4164,9 @@ static void lsp_display_page()
 	if (cf->do_reload)
 		lsp_file_reload();
 
-	if (!lsp_mode_is_toc()) {
-		/* Nothing to display at EOF. */
-		if (cf->size != LSP_FSIZE_UNKNOWN && (lsp_pos == cf->size))
-			return;
-
+	if (!lsp_mode_is_toc())
 		/* Save offset from where we build this page. */
 		cf->page_first = lsp_pos;
-	}
 
 	lsp_invalidate_cm_cursor();
 

commit 5018f2e8f44dfdd6ccb6c03164413ea21132d436

lsp/last page: don't allow movements beyond the last page
Since commit
fcf742f (lsp/display_page: always write complete page, 2025-04-12),
it is possible, to move one page beyond the last one and backward
movements then cause segfaults.

Fix this by:

1) Prevent dereferencing NULL pointers in lsp_wline_bw() when no
   current line could be read.
2) Don't allow movements beyond the last page in lsp_workhorse().

Fixes: fcf742f (lsp/display_page: always write complete page, 2025-04-12)
Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -4715,6 +4715,9 @@ static void lsp_wline_bw(int n)
 	 */
 	struct lsp_line_t *line = lsp_get_this_line();
 
+	if (!line)
+		return;
+
 	/*
 	 * After lsp_get_this_line() lsp_pos is at the character following that
 	 * line; we want it at the beginning of the just read line.
@@ -6262,6 +6265,10 @@ static void lsp_workhorse()
 					/* We stay on last TOC page. */
 					cf->toc = cf->toc_first;
 			}
+
+			if (cf->page_last == cf->size)
+				lsp_file_set_pos(cf->page_first);
+
 			lsp_display_page(); /* next page */
 			break;
 		case 'g':