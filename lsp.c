dgouders/lsp

commit 790547d4c41bc7d47f7cc53d5e7248c82fa501fb

lsp/reloading: enable 'r'eloading the output of git commands
*lsp* already supports to reload the content of manual pages and
regular files.  This means the whole infrastructure to reload any
content is available and now, we extend reload capabilities to other
commands that use *lsp* as a pager.

Currently, it seems too naive to just try to reload any parent
process; in case of command pipes that would even be impossible.

Therefore, we start doing this in a controlled way, enabling reloads
only for commands where we know that in their cases it could be
useful.

The first one will be git(1) commands.
At least in my workflow, I frequently check changes with `git diff`
before doing a commit and very often minor fixes (e.g. whitespace) are
necessary, that always require to leave the currently paged diff and
invoke `git diff` again.

Now, explicit restarts of `git diff` are no longer needed but only
pressing 'r' to reload the diff after having fixed recognized issues.

Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -731,6 +731,16 @@ static bool lsp_is_sgr_sequence(const char *c)
 
 }
 
+static bool lsp_parent_is_restartable(const char *name)
+{
+	int i;
+
+	for (i = 0; lsp_restartable[i]; i++)
+		if (LSP_STR_EQ(name, lsp_restartable[i]))
+			return true;
+	return false;
+}
+
 /*
  * Tell if current position is at the beginning of a line.
  */
@@ -3351,13 +3361,21 @@ static void lsp_file_reread()
 /*
  * Reload content of current file.
  *
- * Currently, explicit reloading is supported only for regular files.
+ * Currently, explicit reloading is supported only for regular files
+ * and for known parents that feed us via stdin.
  *
  */
 static void lsp_cmd_reload()
 {
+	lsp_prompt = lsp_content_reloaded;
+
 	if (lsp_file_is_regular()) {
 		lsp_file_reread();
+		return;
+	}
+
+	if (lsp_parent_is_restartable(lsp_pinfo->argv[0])) {
+		lsp_file_reload();
 		return;
 	}
 
@@ -5055,25 +5073,22 @@ static void lsp_set_pager(const char *pager)
  */
 static void lsp_start_feeder(lsp_feeder_t which_one)
 {
+	int ptmxfd;
 	char **e_argv;
+	char *name;
+	struct winsize winsize;
+	struct termios termios;
 
 	if (which_one == LSP_MAN_COMMAND)
 		e_argv = lsp_create_man_argv(lsp_load_man_command, cf->name);
 	else
 		e_argv = lsp_pinfo->argv;
-	/*
-	 * We want our feeder to see a tty so that it wants to use a pager (us).
-	 */
-	struct winsize winsize;
-	struct termios termios;
 
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &winsize);
 	ioctl(STDOUT_FILENO, TCGETS, &termios);
 
 	if (lsp_do_line_numbers)
 		winsize.ws_col -= 8;
-
-	int ptmxfd;
 
 	/* Look out for situations when there is an uncatched child. */
 	assert(cf->child_pid == 0);
@@ -5100,10 +5115,14 @@ static void lsp_start_feeder(lsp_feeder_t which_one)
 
 	lsp_file_set_blksize();
 
-	/* Try to find a manpage name heading its content. */
-	char *name = lsp_read_manpage_name();
-
 	lsp_file_add_block();
+
+	/* Try to find a manpage name heading its content. */
+	if (!lsp_file_is_manpage())
+		return;
+
+	/* Take care for filename of manpage. */
+	name = lsp_read_manpage_name();
 
 	if (name == NULL)
 		name = lsp_detect_manpage(false);
@@ -5423,19 +5442,40 @@ static void lsp_man_reposition(char *section)
 		 * Use old behavior: use previous page_first and reposition to
 		 * BOL.
 		 */
-		lsp_file_set_pos(cf->page_first);
-		lsp_goto_bol();
+		lsp_file_set_pos_bol(cf->page_first);
 	} else {
 		lsp_file_forward_empty_lines(lsp_reposition.elines);
 		lsp_file_forward_words(lsp_reposition.words);
 	}
 }
 
-/*
- * Currently, only manual pages get reloaded -- no need to check if the current
- * file (cf) is a manual page.
- */
 static void lsp_file_reload()
+{
+	if (lsp_file_is_manpage()) {
+		lsp_file_reload_manpage();
+		return;
+	}
+
+	lsp_file_reset();
+
+	lsp_start_feeder(LSP_PARENT_COMMAND);
+
+	lsp_file_read_to_pos(cf->page_first);
+
+	/*
+	 * Simply go to the last page, if the content shrank so that our
+	 * previous page position isn't longer part of the file.
+	 */
+	if (cf->seek <= cf->page_first) {
+		lsp_cmd_goto_end();
+		cf->page_first = lsp_pos;
+	}
+
+	lsp_file_set_pos_bol(cf->page_first);
+	return;
+}
+
+static void lsp_file_reload_manpage()
 {
 	char *saved_man_section;
 

doc/lsp-help.1
@@ -17,7 +17,7 @@
 .\" You should have received a copy of the GNU General Public License along with
 .\" lsp. If not, see <https://www.gnu.org/licenses/>.
 .\"
-.TH lsp-help 1 01/05/2025 0.5.0-rc4 "lsp online help"
+.TH lsp-help 1 04/12/2025 0.5.0-rc4 "lsp online help"
 .if \n(.g \{\
 .  \" Work around Savannah #65189.
 .  if n .do if \n[cR] .do if ((\n[.x] = 1) & (\n[.y] < 24)) .pl 99999v
@@ -45,7 +45,7 @@ T}
 _
 h;Show this online help.
 _
-r;Reload regular file.
+r;Reload regular file, manual page or git(1) command.
 _
 .T&
 cfI s.

doc/lsp.1
@@ -17,7 +17,7 @@
 .\" You should have received a copy of the GNU General Public License along with
 .\" lsp. If not, see <https://www.gnu.org/licenses/>.
 .\"
-.TH lsp 1 04/06/2025 0.5.0-rc4 "User commands"
+.TH lsp 1 04/12/2025 0.5.0-rc4 "User commands"
 .\"--------------------------------------------------------------------
 .SH Name
 .\"--------------------------------------------------------------------
@@ -100,6 +100,17 @@ extend, but
 .B it might be
 .BR incomplete .
 Users should keep that in mind.
+.P
+.I lsp
+also supports reloading the content of files, but currently only for:
+.IP
+\[bu] manual pages
+.IP
+\[bu] regular files
+.IP
+\[bu]
+.MR git 1
+commands
 .\"--------------------------------------------------------------------
 .SH Searching
 .\"--------------------------------------------------------------------
@@ -601,7 +612,9 @@ exit selection without selecting a file; stay at the former one.
 .br
 Reload current file.
 .IP
-(Currently only for regular files.)
+Currently only manual pages, regular files and the output of
+.MR git 1
+commands can be reloaded.
 .\"--------------------------------------------------------------------
 .SH Environment
 .\"--------------------------------------------------------------------

lsp.h
@@ -152,6 +152,8 @@ struct lsp_parent_info {
 
 struct lsp_parent_info *lsp_pinfo;
 
+char *lsp_restartable[] = {"git", "man", NULL};
+
 typedef enum lsp_feeder {
 	LSP_MAN_COMMAND = 1,	/* use man(1) to start a feeder */
 	LSP_PARENT_COMMAND 	/* use parent cmdline to start a feeder */
@@ -236,6 +238,7 @@ static void			lsp_file_read_all(void);
 static ssize_t			lsp_file_read_block(size_t);
 static void			lsp_file_read_to_pos(off_t);
 static void			lsp_file_reload(void);
+static void			lsp_file_reload_manpage(void);
 static void			lsp_file_reread(void);
 static void			lsp_file_reset(void);
 static void			lsp_file_ring_dtor(void);
@@ -318,6 +321,7 @@ static size_t			lsp_normalize_count(const char *, size_t, size_t);
 static void			lsp_open_cterm(void);
 static int			lsp_open_file(const char *);
 static void			lsp_open_manpage(char *);
+static bool			lsp_parent_is_restartable(const char *);
 static void			lsp_pinfo_dtor(void);
 static void			lsp_pinfo_ctor(void);
 #if DEBUG
@@ -583,6 +587,7 @@ unsigned char lsp_shift;
 /* xxx put into lsp_init() or so */
 char	*lsp_not_found = "Pattern not found";
 char    *lsp_reload_not_supported = "Reload not supported.";
+char	*lsp_content_reloaded = "File content reloaded.";
 
 /* String from LSP_OPEN or LESSOPEN environment variable. */
 char *lsp_env_open;

commit c5fd8460e60a1e89f2ef05a3b0b8aabc50a1e79d

lsp/reloading: fix reloading of manual pages
The next fix of prior work:

Commit 790547d (lsp/reloading: enable 'r'eloading the output of git
commands, 2025-04-10) broke the reloading of manual pages.

In lsp_start_feeder(), an initial data block is now read regardless of
the file type.  But, for manual pages, we first need to process the
output of lsp_cat(1) with MAN_PN as its first line.

Now, we don't get the name of the manual page out of the first line of
input, because we try to read that initial line after a first block
was read;  the result is completely unpredictable but by chance was
hardly noticeable from the user's perspective, which allowed that
bug to hide for some time.

(The reason, it was noticed, are manual pages with a dot in their names,
 like sys_un.h(0P).  MAN_PN escapes that dot with a backslash which we
 do not yet correct and that backslash suddenly disappeared after
 reloading because of resizes.)

Correct this by immediately reading initial data blocks only for files
that aren't manual pages and for manual pages, call
lsp_read_manpage_name() prior to reading the initial data block.

Fixes: 790547d (lsp/reloading: enable 'r'eloading the output of git commands, 2025-04-10)
Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -5117,14 +5117,16 @@ static void lsp_start_feeder(lsp_feeder_t which_one)
 
 	lsp_file_set_blksize();
 
-	lsp_file_add_block();
-
-	/* Try to find a manpage name heading its content. */
-	if (!lsp_file_is_manpage())
+	/* If cf isn't a manual page, just read an initial block. */
+	if (!lsp_file_is_manpage()) {
+		lsp_file_add_block();
 		return;
+	}
 
 	/* Take care for filename of manpage. */
 	name = lsp_read_manpage_name();
+
+	lsp_file_add_block();
 
 	if (name == NULL)
 		name = lsp_detect_manpage(false);