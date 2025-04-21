dgouders/lsp

commit 0fd7bbeacbff1f8e09a7f7fde77989bfe8193794

options: implement --reload-command
For this change we had to move from execlp() to execvp().

A helper function lsp_create_argv() was also implementet; we can use
it for --verify-command as well.

Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -3839,6 +3839,42 @@ static void lsp_open_manpage(char *name)
 	lsp_exec_man();
 }
 
+/*
+ * Create argument vector for exec().
+ *
+ * For now, we expect a format string that contains exactly one "%s" to be
+ * replaced by the second argument str.
+ */
+char** lsp_create_argv(char *format, char *str)
+{
+	char **argv = malloc(sizeof(char *));
+	char *tok;
+	unsigned int i = 0;
+
+	lsp_debug("%s: building argv for \"%s\" and \"%s\"", __func__, format, str);
+
+	/* Get first token. */
+	tok = strtok(strdup(format), " \t");
+
+	while (tok) {
+		if (!strcmp(tok, "%s"))
+			argv[i] = str;
+		else
+			argv[i] = tok;
+
+		/* Get next token. */
+		tok = strtok(NULL, " \t");
+
+		i++;
+		argv = realloc(argv, (i + 1) * sizeof(char *));
+	}
+
+	/* Terminate argv. */
+	argv[i] = NULL;
+
+	return argv;
+}
+
 static void lsp_exec_man()
 {
 	/*
@@ -3864,8 +3900,10 @@ static void lsp_exec_man()
 
 		putenv("PAGER=cat");
 
-		execlp("man", "man", cf->name, (char *)NULL);
-		lsp_error("execlp() failed.\n");
+		char **e_argv = lsp_create_argv(lsp_reload_command, cf->name);
+
+		execvp(e_argv[0], e_argv);
+		lsp_error("execvp() failed.\n");
 	}
 
 	cf->fd = ptmxfd;
@@ -5356,7 +5394,7 @@ static void lsp_init()
 	lsp_load_apropos = false;
 	lsp_apropos_command = strdup("apropos . | sort | sed 's/ (/(/'");
 
-	lsp_reload_command = strdup("man");
+	lsp_reload_command = strdup("man %s");
 
 	lsp_verify_command = strdup("man -w %s");
 

lsp.h
@@ -137,6 +137,7 @@ static void			lsp_cmd_toggle_options(void);
 static void			lsp_cmd_visit_reference(void);
 static char *			lsp_cmd_select_file(void);
 static int			lsp_cmp_line_pos(size_t, off_t);
+static char**			lsp_create_argv(char *, char *);
 static struct man_id		lsp_create_man_id(const char *);
 static void			lsp_create_status_line(void);
 static void			lsp_cursor_care(void);

commit a5ba54cd7794b6b97425863620606deb454eae89

lsp_start_feeder(): fix a memory leak
lsp_start_feeder() doesn't care to probably free e_argv after it has
been used to create the feeder process.

But the main problem with the code is:

* e_argv is needed in the child only and because the child calls
  execvp(3) after e_argv is prepared, there is no problem with
  cleanup in the child code.

Move the preparation of e_argv into the child-code so that the parent
hasn't to care about if memory has to be free()'d.

Fixes: 0fd7bbe (options: implement --reload-command, 2023-03-29)
Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -5081,11 +5081,6 @@ static void lsp_start_feeder(lsp_feeder_t which_one)
 	struct winsize winsize;
 	struct termios termios;
 
-	if (which_one == LSP_MAN_COMMAND)
-		e_argv = lsp_create_man_argv(lsp_load_man_command, cf->name);
-	else
-		e_argv = lsp_pinfo->argv;
-
 	ioctl(STDOUT_FILENO, TIOCGWINSZ, &winsize);
 	ioctl(STDOUT_FILENO, TCGETS, &termios);
 
@@ -5105,6 +5100,11 @@ static void lsp_start_feeder(lsp_feeder_t which_one)
 
 	if (cf->child_pid == 0) {		/* child process */
 		lsp_set_pager("lsp_cat");
+
+		if (which_one == LSP_MAN_COMMAND)
+			e_argv = lsp_create_man_argv(lsp_load_man_command, cf->name);
+		else
+			e_argv = lsp_pinfo->argv;
 
 		execvp(e_argv[0], e_argv);
 		lsp_error("%s: execvp() failed.", __func__);