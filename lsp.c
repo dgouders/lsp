dgouders/lsp

commit 6711c00980e4588df4fe1073e4d3ef9733a2946a

lsp: introduce pinfo -- information to restart our parent
To restart the process that started us, we need to know its command
line.  Get this information and store it in the new structure
lsp_pinfo.

For now, this structure is a global one, because we only have one
parent process.  Could be that we later want this per file to be able
to restart processes that feed us data for various open files.
But currently, we are only able to open additional manual pages and we
already know how to restart man(1).

Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -6559,6 +6559,8 @@ static void lsp_finish()
 	free(lsp_verify_command);
 	free(lsp_apropos_command);
 
+	lsp_pinfo_dtor();
+
 	exit(EXIT_SUCCESS);
 }
 
@@ -6640,6 +6642,8 @@ static void lsp_init()
 
 	lsp_hwin = NULL;
 	lsp_hwin_cols = -1;
+
+	lsp_pinfo_ctor();
 }
 
 #if DEBUG
@@ -6680,6 +6684,103 @@ static void lsp_argv_dtor(char **argv)
 		free(argv[i++]);
 
 	free(argv);
+}
+
+static unsigned int lsp_ndigits(unsigned int n)
+{
+	unsigned int digits = 1;
+
+	while (n > 9) {
+		n /= 10;
+		digits++;
+	}
+
+	return digits;
+}
+
+/*
+ * Execute command given in cmd and return its output as a string.
+ *
+ * Return NULL on failure.
+ */
+static char *lsp_run_command2str(char *cmd)
+{
+	/* Size of chunks to read. */
+	size_t r_len = 64;
+
+	/* Result string and its current length. */
+	size_t b_len = r_len;
+	char *buffer = lsp_malloc(b_len);
+
+	size_t nread = 0;
+
+	FILE *fp = popen(cmd, "r");
+
+	if (fp == NULL)
+		lsp_error("%s: could not popen(\"%s\").", __func__, cmd);
+
+	/*
+	 * Read command output and store it in the result string.
+	 */
+	while (!feof(fp)) {
+		size_t chunk = (buffer + nread) - (buffer + b_len);
+
+		if (!chunk) {
+			b_len += r_len;
+
+			buffer = lsp_realloc(buffer, b_len);
+
+			chunk = r_len;
+		}
+
+		/* Ensure string termination. */
+		buffer[nread] = '\0';
+
+		nread += lsp_fread(buffer + nread, 1, chunk, fp);
+	}
+
+	/* Maybe we are wasting some bytes, here.  The length is <= 64 and
+	   IMO not worth a realloc(). */
+
+	pclose(fp);
+
+	/* Remove trailing newline. */
+	if (nread && buffer[nread - 1] == '\n')
+		buffer[nread - 1] = '\0';
+
+	return buffer;
+}
+
+static char *lsp_get_parent_cmd_line(pid_t pid)
+{
+	char *ps_cmd = "ps -p %u -o args=";
+	char *cmd = lsp_malloc(strlen(ps_cmd) - 2 + lsp_ndigits(pid) + 1);
+	sprintf(cmd, ps_cmd, pid);
+	return lsp_run_command2str(cmd);
+}
+
+/*
+ * Create structure for information about our parent process.
+ */
+static void lsp_pinfo_ctor()
+{
+	lsp_pinfo = lsp_malloc(sizeof(*lsp_pinfo));
+
+	lsp_pinfo->pid = getppid();
+	lsp_pinfo->cmd_line = lsp_get_parent_cmd_line(lsp_pinfo->pid);
+	lsp_pinfo->argv = lsp_str2argv(lsp_pinfo->cmd_line);
+}
+
+static void lsp_pinfo_dtor()
+{
+	if (!lsp_pinfo)
+		return;
+
+	free(lsp_pinfo->cmd_line);
+	lsp_argv_dtor(lsp_pinfo->argv);
+
+	free(lsp_pinfo);
+	lsp_pinfo = NULL;
 }
 
 int main(int argc, char *argv[])

lsp.h
@@ -140,6 +140,17 @@ enum lsp_mode {
 };
 
 typedef enum lsp_mode lsp_mode_t;
+
+/*
+ * Information needed to restart parent processes.
+ */
+struct lsp_parent_info {
+	char *cmd_line;
+	pid_t pid;
+	char **argv;
+};
+
+struct lsp_parent_info *lsp_pinfo;
 
 static void			lsp_apropos_create_grefs(void);
 static void			lsp_argv_dtor(char **);
@@ -222,6 +233,7 @@ static struct gref_t *		lsp_get_gref_at_pos(regmatch_t);
 static struct lsp_line_t *	lsp_get_line_at_pos(off_t);
 static struct lsp_line_t *	lsp_get_line_from_here(void);
 static struct lsp_line_t *	lsp_get_next_display_line(void);
+static char *			lsp_get_parent_cmd_line(pid_t);
 static size_t			lsp_get_sgr_len(const char *);
 static struct lsp_line_t *	lsp_get_this_line(void);
 static struct gref_t *		lsp_gref_find(char *);
@@ -279,6 +291,8 @@ static size_t			lsp_normalize_count(const char *, size_t, size_t);
 static void			lsp_open_cterm(void);
 static int			lsp_open_file(const char *);
 static void			lsp_open_manpage(char *);
+static void			lsp_pinfo_dtor(void);
+static void			lsp_pinfo_ctor(void);
 #if DEBUG
 static void			lsp_print_file_ring(void);
 #endif

commit 3085f3cc992c6e830709a8491bfc0ad195fe6030

lsp_get_parent_cmd_line(): fix a memory leak
In lsp_get_parent_cmd_line(), the command used to get the parent
command line should be free()'d when it's no longer needed.

While at it, polish that short function a bit and, for example, use a
variable name that expresses we have a format string, instead of
"ps_cmd".

Fixes: 6711c00 (lsp: introduce pinfo -- information to restart our parent, 2024-07-22)
Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -7231,10 +7231,18 @@ static char *lsp_run_command2str(char *cmd)
 
 static char *lsp_get_parent_cmd_line(pid_t pid)
 {
-	char *ps_cmd = "ps -p %u -o args=";
-	char *cmd = lsp_malloc(strlen(ps_cmd) - 2 + lsp_ndigits(pid) + 1);
-	sprintf(cmd, ps_cmd, pid);
-	return lsp_run_command2str(cmd);
+	char *result;
+	char *cmd_fmt = "ps -p %u -o args=";
+	char *cmd;
+
+	cmd = lsp_malloc((strlen(cmd_fmt) - 2) + lsp_ndigits(pid) + 1);
+
+	sprintf(cmd, cmd_fmt, pid);
+
+	result = lsp_run_command2str(cmd);
+
+	free(cmd);
+	return result;
 }
 
 /*