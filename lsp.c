3719accf8a4ec81153558db159216d2c49583b25
Dirk Gouders <dirk@gouders.net>
2025-10-30 22:17:15 +0100

    lsp_line_add_wlines: column 2 can happen with wchars
    Given the proper input, we actually can reach column 2.
    
    Fix the assert() to not crash on such input.
    
    Fixes: #35
    Signed-off-by: Dirk Gouders <dirk@gouders.net>

--- lsp.c
+++ lsp.c
@@ -1442,7 +1442,7 @@ static void lsp_line_add_wlines(struct lsp_line_t *line)
 		current_col = col;
 
 		if (col >= lsp_maxx || row > 0) {
-			assert(col <= 1);
+			assert(col <= 2);
 			col = 0;
 			row = 0;
 			wmove(lsp_hwin, row, col);
