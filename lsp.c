dgouders/lsp

commit ee7b96ff4b083475ab7f9ac2ccf415f28762335e

lsp_normalize: null-terminate returned data
Sporadic bus errors were noticed, while searches were active.

When searching, regexec(3) is used to search in the normalized data of
lines and that data isn't null-terminated, but regexec(3) requires
that data to be null-terminated.

Fix this in lsp_normalize() so that null-terminated data will be
returned.

The term "null-terminated data" is used, because in case of binary
input, that data itself can contain null-characters.

Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -1584,9 +1584,10 @@ static size_t lsp_normalize_count(const char *raw, size_t raw_len, size_t length
  *
  * SGR sequences are also recognized and ignored.
  *
- * Note: this function returns data that is _not_ null-terminated, i.e. no
- *       string.  This would be meaningless, because the normalized data itself
- *       can contain null-characters.
+ * Because we need to be able to run regexec(3) on the results of this function,
+ * the returned data will be null-terminated, i.e. it looks like a string.
+ * But when we have to deal with binary input, that string itself can
+ * contain null-characters!
  */
 static char *lsp_normalize(const char *raw, size_t raw_len, size_t *n_length)
 {
@@ -1596,9 +1597,10 @@ static char *lsp_normalize(const char *raw, size_t raw_len, size_t *n_length)
 	size_t i;
 
 	/* We should be allocating too much memory, because the worst we do is
-	   to ignore characters from the raw data.
-	   We correct the allocated size below. */
-	normalized = lsp_malloc(raw_len);
+	 * that we _ignore_ characters from the raw data;
+	 * we correct the allocated size below.
+	 */
+	normalized = lsp_malloc(raw_len + 1);
 
 	/* Copy the data ignoring c\b sequences */
 	for (i = 0, nlen = 0; i < raw_len; i += ch_len) {
@@ -1613,11 +1615,12 @@ static char *lsp_normalize(const char *raw, size_t raw_len, size_t *n_length)
 		/* Append the char to normalized data. */
 		memcpy(normalized + nlen, raw + i, ch_len);
 		nlen += ch_len;
+		normalized[nlen] = '\0';
 	}
 
 	/* Adjust the allocated memory to the correct size */
 	if (raw_len > nlen)
-		normalized = lsp_realloc(normalized, nlen);
+		normalized = lsp_realloc(normalized, nlen + 1);
 
 	/* ...or error out if our heuristics failed. */
 	if (raw_len < nlen)

commit 10cdc74e842b8f4e5278efe3504f831d7e6cf1a6

line.normalized: fix leftovers from null-terminating it
There are some overseen parts, ee7b96f (lsp_normalize: null-terminate
returned data, 2025-12-03) should have taken care of:

- A comment in lsp_line_t has been overseen.
  Fix that comment.

- A null-terminated .normalized partly requires toggled usage of
  lsp_mdup() vs. lsp_mdup2str().

- lsp_line_get_matches() removes the final newlines in .normalized and
  we should ensure .normalized always has one.

- When we add content to a line, comparisons with cf->size
  should allow that the other part could become larger (not just equal).

Fixes: ee7b96f (lsp_normalize: null-terminate returned data, 2025-12-03)
Signed-off-by: Dirk Gouders <dirk@gouders.net>

lsp.c
@@ -1543,6 +1543,11 @@ static struct lsp_line_t *lsp_get_line_from_here()
 		ret = lsp_file_getch(&ch);
 	}
 
+	/* Ensure for a final newline in each line we return --
+	   even if the file has no trailing one. */
+	if (ret == -1)
+		str[pos++] = '\n';
+
 	/* Reallocate to correct size */
 	str = lsp_realloc(str, pos + 1);
 
@@ -4041,7 +4046,7 @@ static size_t lsp_line_get_matches(const struct lsp_line_t *line, regmatch_t **p
 	 *
 	 * Duplicate the normalized line and remove the final \n.
 	 */
-	char *sstring = lsp_mdup(line->normalized, line->nlen - 1);
+	char *sstring = lsp_mdup2str(line->normalized, line->nlen - 1);
 	size_t slen = line->nlen - 1;
 
 	/* Allocate memory for max possible number of matches and that
@@ -4975,7 +4980,7 @@ static void lsp_cmd_forward(int n)
 	/* Rewind to first byte in the page */
 	lsp_file_set_pos(cf->page_first);
 
-	if (cf->page_last == cf->size)
+	if (cf->size != LSP_FSIZE_UNKNOWN && cf->page_last >= cf->size)
 		return;
 
 	if (lsp_chop_lines != 0) {
@@ -5644,7 +5649,7 @@ static char *lsp_man_get_section(off_t pos)
 	if (line->pos == 0)
 		section_name = strdup("_start_of_manual_page_");
 	else
-		section_name = lsp_mdup2str(line->normalized, line->nlen);
+		section_name = lsp_mdup(line->normalized, line->nlen + 1);
 
 	lsp_line_dtor(line);
 
@@ -6582,7 +6587,7 @@ static void lsp_workhorse()
 					cf->toc = cf->toc_first;
 			}
 
-			if (cf->page_last == cf->size)
+			if (cf->size != LSP_FSIZE_UNKNOWN && cf->page_last >= cf->size)
 				lsp_file_set_pos(cf->page_first);
 
 			lsp_display_page(); /* next page */

lsp.h
@@ -101,10 +101,17 @@ struct toc_node_t {
  * The member .normalized is for data without formatting information,
  * i.e. backspace and SGR sequences.
  *
- * Note: .raw and .normalized are *not* strings with null-terminators.
- *       The reason is that users can feed us any data in the range [0x00-0xff]
- *       -- null-terminators for .raw and .normalized would be meaningless and
- *       actually just complicate things.
+ * Note: .raw is *not* a string with a null-terminator.
+ *	 The reason is that users can feed us any data in the range [0x00-0xff]
+ *       -- a null-terminator for .raw would be meaningless and actually just
+ *       complicate things.
+ *	 On the other side, .normalized is null-terminated but should not be
+ *       seen as an ordinary string, because -- depending on the input -- the
+ *       data in it can contain null-characters.  For this reason, we have nlen
+ *       holding the length of .normalized.
+ *	 Yes, we match regular expressions on it, but with having in mind that
+ *       regexec(3) probably doesn't test the whole length of .normalized,
+ *       because it might see a null-character before the real null-terminator.
  *
  * Lines might be longer than the window width.  So, we also maintain pointers
  * to "wlines".  Currently, this overhead just makes scrolling backwards